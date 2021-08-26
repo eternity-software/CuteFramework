d927905bb3f0db4520554a1831a9ca66c25771a6
karlov <51130679+karlovm@users.noreply.github.com>
2021-08-26 21:04:25 +0300

    Fixing

--- src/ru/etysoft/cuteframework/methods/media/UploadImageRequest.java
+++ src/ru/etysoft/cuteframework/methods/media/UploadImageRequest.java
@@ -7,12 +7,13 @@ import ru.etysoft.cuteframework.data.APIMethods;
 import ru.etysoft.cuteframework.exceptions.ResponseException;
 import ru.etysoft.cuteframework.requests.POST;
 import ru.etysoft.cuteframework.requests.RequestHolder;
+import ru.etysoft.cuteframework.requests.attachements.ImageFile;
 
 import java.util.HashMap;
 
 public class UploadImageRequest extends RequestHolder {
-    private Object file;
-    public UploadImageRequest(Object file) {
+    private ImageFile file;
+    public UploadImageRequest(ImageFile file) {
         super(APIMethods.Media.UP_LOAD_IMAGE);
         this.file = file;
     }
